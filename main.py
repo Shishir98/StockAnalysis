88b9a3d5b60fa44dfd4ce8a3d9c9a4f6946e46cb
Shishir Saxena <shishir98@live.com>
2022-09-28 23:43:42 +0530

    Created anamoly detection class and created functions for scalar transforms and fitting model.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from Stock_Visualised import StockAnalysis
-from pre_processing import Training
+from pre_processing import Training, Anamoly
 
 DATA_DIR = "StockData/JNJ.csv"
+TEST_DATA_DIR = "StockData/BSE Sensex.csv"
 TIME_STEPS = 15
 TRAIN_TEST_SIZE = 0.9
 LEARNING_RATE = 0.001
@@ -11,10 +12,11 @@ VALIDATION_SPLIT = 0.1
 LOSS = 'mae'
 
 
-def train_base_model(data_directory, time_steps, train_test_size, learning_rate, epoch, batch_size, validation_split, loss):
+def train_base_model(data_directory, time_steps, train_test_size, learning_rate, epoch, batch_size, validation_split,
+                     loss):
     data = StockAnalysis(data_directory)
     dataset = data.read_csv()
-    preprocess = Training(dataset,train_test_size, learning_rate, epoch, batch_size, validation_split, loss)
+    preprocess = Training(dataset, train_test_size, learning_rate, epoch, batch_size, validation_split, loss)
     train, test = preprocess.scaler_transforms()
     X_train, y_train = preprocess.create_dataset(train[['Close']], train.Close, time_steps)
     X_test, y_test = preprocess.create_dataset(test[['Close']], test.Close, time_steps)
@@ -22,4 +24,17 @@ def train_base_model(data_directory, time_steps, train_test_size, learning_rate,
     history = preprocess.start_training(model, callbacks, X_train, y_train)
 
 
-train_base_model(DATA_DIR, TIME_STEPS, TRAIN_TEST_SIZE, LEARNING_RATE, EPOCH, BATCH_SIZE, VALIDATION_SPLIT, LOSS)
+def detect_anamoly(data_directory, time_steps, train_test_size, learning_rate, epoch, batch_size, validation_split,
+                   loss):
+    preprocess = Training(data_directory, train_test_size, learning_rate, epoch, batch_size, validation_split, loss)
+    anamoly = Anamoly(data_directory, train_test_size, learning_rate, epoch, batch_size, validation_split, loss)
+    train_scalar, test_scalar = anamoly.scalar_transforms()
+    x_test_scalar, y_test_scalar = preprocess.create_dataset(test_scalar[['Close']], test_scalar.Close, time_steps)
+    x_train_scalar, y_train_scalar = preprocess.create_dataset(train_scalar[['Close']], train_scalar.Close,
+                                                           time_steps)
+    fit_history = anamoly.fit_model(x_train_scalar, y_train_scalar)
+
+
+# train_base_model(DATA_DIR, TIME_STEPS, TRAIN_TEST_SIZE, LEARNING_RATE, EPOCH, BATCH_SIZE, VALIDATION_SPLIT, LOSS)
+
+detect_anamoly(TEST_DATA_DIR, TIME_STEPS, TRAIN_TEST_SIZE, LEARNING_RATE, EPOCH, BATCH_SIZE, VALIDATION_SPLIT, LOSS)
